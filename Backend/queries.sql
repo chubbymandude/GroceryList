bc0614fb3d955e745891f2da82db905a99020ff1
chubbymandude <abdurrafayatif1@gmail.com>
2025-08-11 17:16:03 -0400

    updated sql file

--- Backend/queries.sql
+++ Backend/queries.sql
@@ -4,16 +4,17 @@ DROP TABLE groceries;
 /* create table which consists of information about a user's groceries */
 CREATE TABLE groceries
 (
-	item_name text PRIMARY KEY,
+	id bigserial PRIMARY KEY,
+	item_name text,
 	price numeric(5, 2), /* grocery items cannot exceed $999.9 */
-	quantity integer,
+	quantity integer, 
 	on_sale boolean,
 	CONSTRAINT item_name_key UNIQUE (item_name) /* items can be searched by their name */
 );
 
 /* import from a local file */
-COPY groceries
-FROM '...'
+COPY groceries (item_name, price, quantity, on_sale)
+FROM '/Users/abdurrafayatif/Downloads/Right Angle Solutions/Groceries/Backend/Grocery List - Groceries.csv'
 WITH (FORMAT CSV, HEADER);
 
 /* check that table has correct items */
@@ -23,6 +24,6 @@ SELECT * FROM groceries;
 CREATE INDEX item_index ON groceries(item_name);
 
 /* check if index is being used */
-EXPLAIN SELECT item_name
-FROM groceries
+EXPLAIN SELECT item_name 
+FROM groceries 
 WHERE item_name = 'Apples';
